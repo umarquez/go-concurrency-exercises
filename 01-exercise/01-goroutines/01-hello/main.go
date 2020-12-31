8fe3b1f7f99ad3a3ff3fcfad6c7734082401a422
Uriel Márquez <um@c0d3.mx>
2020-12-31 08:19:25 +0000

    01-exercise/01-goroutines/01-hello/ solved

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+const sleepTime = 1000
+
 func fun(s string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
@@ -19,12 +21,19 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
+  go fun("goroutine function call")
 
 	// goroutine with anonymous function
+  go func () {
+    fun("anonymous")
+  }()
 
 	// goroutine with function value call
+  fn := fun
+  go fn("value call")
 
 	// wait for goroutines to end
-
+  fmt.Printf("waiting %vms...\n", sleepTime)
+  time.Sleep(time.Millisecond*sleepTime)
 	fmt.Println("done..")
 }
