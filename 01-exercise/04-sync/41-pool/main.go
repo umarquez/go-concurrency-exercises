f0321603cae08aec186965faa2cd9f47dff6e19b
Uriel Márquez <um@c0d3.mx>
2021-01-03 06:18:43 +0000

    + ex. 41 solved

--- 01-exercise/04-sync/41-pool/main.go
+++ 01-exercise/04-sync/41-pool/main.go
@@ -5,12 +5,23 @@ import (
 	"io"
 	"os"
 	"time"
+  "sync"
+  "fmt"
 )
 
 //TODO: create pool of bytes.Buffers which can be reused.
 
+var bufPool = sync.Pool{
+  New: func() interface{}{
+    fmt.Println("Init buff") // only runs once
+    return new(bytes.Buffer)
+  },
+}
+
 func log(w io.Writer, val string) {
-	var b bytes.Buffer
+	var b = bufPool.Get().(*bytes.Buffer)
+
+  b.Reset()
 
 	b.WriteString(time.Now().Format("15:04:05"))
 	b.WriteString(" : ")
@@ -18,6 +29,7 @@ func log(w io.Writer, val string) {
 	b.WriteString("\n")
 
 	w.Write(b.Bytes())
+  bufPool.Put(b)
 }
 
 func main() {
